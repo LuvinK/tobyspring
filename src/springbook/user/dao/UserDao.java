ff515d2d9715b1d2c7e0a10c6a01a9d990ee61da
hyunggyu cho <tyche77@gmail.com>
2012-09-28 09:51:08 +0900

    - UserDao main 메소드 추가 - UserDao 오타수정 - Maven Project로 변환

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -25,7 +25,7 @@ public class UserDao {
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql//localhost/tobyspring", "tobyspring", "wjrmaxhd");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/tobyspring", "tobyspring", "wjrmaxhd");
 		
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		
@@ -43,5 +43,24 @@ public class UserDao {
 		c.close();
 		
 		return user;
+	}
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		UserDao dao = new UserDao();
+		
+		User user = new User();
+		user.setId("whiteship");
+		user.setName("백기선");
+		user.setPassword("married");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId() + " 등록성공");
+		
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		
+		System.out.println(user2.getId() + " 조회성공");	
 	}
 }
