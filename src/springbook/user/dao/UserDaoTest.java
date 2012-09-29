bffbc7983d9be608152de3221c14eef5179e3b54
hyunggyu cho <tyche77@gmail.com>
2012-09-29 19:09:16 +0900

    Ioc 적용  - DaoFactory 생성하여 UserDaoTest의 관심사 분리.

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -6,20 +6,19 @@ import springbook.user.domain.User;
 
 public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		ConnectionMaker connectionMaker = new GConnectionMaker();
-		
-		UserDao dao = new UserDao(connectionMaker);
+
+		UserDao userDao = new DaoFactory().userDao();
 		
 		User user = new User();
 		user.setId("whiteship");
 		user.setName("백기선");
 		user.setPassword("married");
 		
-		dao.add(user);
+		userDao.add(user);
 		
 		System.out.println(user.getId() + " 등록성공");
 		
-		User user2 = dao.get(user.getId());
+		User user2 = userDao.get(user.getId());
 		System.out.println(user2.getName());
 		System.out.println(user2.getPassword());
 		
